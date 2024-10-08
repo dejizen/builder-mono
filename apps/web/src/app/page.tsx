aa2b84f8b82d619c8d09644a8e5900dac04cfd81
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2024-10-08 23:14:41 +0200

    remove useless console log

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,6 @@
 import Builder from "@repo/builder/builder";
 
 export default async function Home() {
-  console.log(
-    "process.env.DATO_CMS_TOKEN",
-    process.env.NEXT_PUBLIC_DATO_CMS_TOKEN
-  );
   return (
     <Builder
       datoCmsToken={process.env.NEXT_PUBLIC_DATO_CMS_TOKEN || ""}
