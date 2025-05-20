54d3a297474c351a1731e96fa404c8e8bf37f6f4
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2025-05-20 16:45:32 +0200

    fix

--- packages/builder/src/components/dynamic-comp.tsx
+++ packages/builder/src/components/dynamic-comp.tsx
@@ -12,31 +12,42 @@ export function DynamicComp(props: DynamicCompoProps) {
     props.dejiComponent.name
   ]
     ? props.customComponents[props.dejiComponent.name]
-    : ''
-  // dynamic(
-  //     () => import(`./builderComps/deji${props.dejiComponent.name}`, {})
-  //   )
+    : null
 
-  return props.dejiComponent.props.enablePreview ? (
-    <div
-      data-kontent-item-id={props.dejiComponent.props.item.system.id}
-      data-kontent-language-codename={
-        props.dejiComponent.props.locale || 'default'
-      }
-    >
+  if (!DynamicComponent) {
+    console.log('component not found', props.dejiComponent.name)
+    return null
+  }
+
+  try {
+    return props.dejiComponent.props.enablePreview ? (
+      <div
+        data-kontent-item-id={props.dejiComponent.props.item.system.id}
+        data-kontent-language-codename={
+          props.dejiComponent.props.locale || 'default'
+        }
+      >
+        <DynamicComponent
+          {...{
+            item: props.dejiComponent.props.item,
+            extra: props.dejiComponent.props.extra,
+          }}
+        />
+      </div>
+    ) : (
       <DynamicComponent
         {...{
           item: props.dejiComponent.props.item,
           extra: props.dejiComponent.props.extra,
         }}
       />
-    </div>
-  ) : (
-    <DynamicComponent
-      {...{
-        item: props.dejiComponent.props.item,
-        extra: props.dejiComponent.props.extra,
-      }}
-    />
-  )
+    )
+  } catch (e) {
+    console.log(
+      'component not found',
+      props.customComponents[props.dejiComponent.name]
+    )
+    console.error(e)
+    return <></>
+  }
 }
